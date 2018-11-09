bcf1db9bf1395c94efc56804568e0615321a0c8b
Hesham Mohsen <30447571+HeshamMohsen@users.noreply.github.com>
2018-11-09 16:32:20 +0200

    add generate function

--- generate.php
+++ generate.php
@@ -10,6 +10,32 @@ $fileThree  = fopen("file3.txt", "w") or die("Unable to open file!");
 $fileFour   = fopen("file4.txt", "w") or die("Unable to open file!");
 $fileFive   = fopen("file5.txt", "w") or die("Unable to open file!");
 
+// generate function takes file, lengthOfFile, charsArray 
+// and generate string from charSet array randomly 
+// then write it in each files 
+function generate ($file, $chars) {
+  // get random file length of characters 
+  $fileLength = rand(10, 30);
+  // define empty string to add the generated characters
+  $str = '';
+  // generate random number from 0 - length of characters array
+  // and then add to str string 
+  for($i = 0; $i < $fileLength; $i++) {
+    $index = rand(0, count($chars) - 1);
+    $str.= $chars[$index] . ' ';
+  }
+  // write str in the opened file
+  fwrite($file, $str);
+  // close the file
+  fclose($file);
+}
+
+// generate function call 5 times of 5 files
+generate($fileOne, $charSet);
+generate($fileTwo, $charSet);
+generate($fileThree, $charSet);
+generate($fileFour, $charSet);
+generate($fileFive, $charSet);
 
 
 ?>
