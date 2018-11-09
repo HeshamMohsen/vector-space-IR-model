e75c8fb6662aef13f162e1425b3467e73d6adac5
Hesham Mohsen <30447571+HeshamMohsen@users.noreply.github.com>
2018-11-09 16:31:42 +0200

    add fopen functions to all five-files

--- generate.php
+++ generate.php
@@ -3,6 +3,13 @@
 // characters array from [A-Z]
 $charSet = ['A','B','C','D','E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+// open  5-files and add flag 'w' => 'write'
+$fileOne    = fopen("file1.txt", "w") or die("Unable to open file!");
+$fileTwo    = fopen("file2.txt", "w") or die("Unable to open file!");
+$fileThree  = fopen("file3.txt", "w") or die("Unable to open file!");
+$fileFour   = fopen("file4.txt", "w") or die("Unable to open file!");
+$fileFive   = fopen("file5.txt", "w") or die("Unable to open file!");
+
 
 
 ?>
