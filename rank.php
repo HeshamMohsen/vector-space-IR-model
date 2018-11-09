e6fb36744da27d3e0b05c9596405bc07324d834a
Hesham Mohsen <30447571+HeshamMohsen@users.noreply.github.com>
2018-11-09 16:59:50 +0200

    add html handler to regex action

--- rank.php
+++ rank.php
@@ -10,14 +10,44 @@ $queryUpper = strtoupper($queryLower);
 
 // check if query empty [rewrite] else check regular expression
 if ($queryUpper == "") {
-    echo 'error, empty query!';
+  $title = "Whoops...";
+  $message = "We're unable to find data you're looking for";
+  $btnText = "Find Again";
   // regular expression that accepts characters from a-z with one white-space in between
-} else if (!preg_match('/^([A-Za-z][ ]{1})+$/', $queryUpper)) {
-    echo 'error, not match!';
+} else if (!preg_match('/^([A-Za-z][ ]{1})+$/', $queryUpper)){
+    $title = "Whoops...";
+    $message = "We're unable to find data you're looking for";
+    $btnText = "Find Again";
 } else { 
-    // start apply vector-space-model functions
+    // apply vector-space-model functions
 }
 
+  if( isset($title) && isset($message) && isset($btnText) ) {
+    $htmlStructure = '
+    <div class="handler--white">
+      <div class="header-text">
+        <div class="header-square">
+          <h1>' . $title . '</h1>
+          <p>' . $message . '</p>
+          <a href="http://localhost/information-retrieval/statistical-model/index.php">' . $btnText . '</a>
+        </div>
+      </div>
+      <div class="image">
+        <div class="image--square">
+            <img src="images/file.PNG"/>
+        </div>
+      </div>
+    </div>';
+  }
 ?>
 
 
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="css/normalize.css"> 
+    <link rel="stylesheet" href="css/ir.css">
+  <body>
+      <?php if(isset($htmlStructure)) echo $htmlStructure ?>
+  </body>
+</html>
